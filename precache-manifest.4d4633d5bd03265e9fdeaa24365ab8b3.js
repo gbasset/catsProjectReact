e9ecec6d1b2171d19fe8c48e6619569ae0a6eaf8
gaetan <gbassetprotonmail.com>
2020-06-07 15:18:27 +0200

    Updates

---
+++ precache-manifest.4d4633d5bd03265e9fdeaa24365ab8b3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860374f1a83b56dfdf4f91346cd0ad5a",
+    "url": "/catsProjectReact/index.html"
+  },
+  {
+    "revision": "72a4153c0416735b8964",
+    "url": "/catsProjectReact/static/css/main.ba719981.chunk.css"
+  },
+  {
+    "revision": "750500e32c68ede850a8",
+    "url": "/catsProjectReact/static/js/2.1e84dc62.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/catsProjectReact/static/js/2.1e84dc62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a4153c0416735b8964",
+    "url": "/catsProjectReact/static/js/main.9f896af9.chunk.js"
+  },
+  {
+    "revision": "9e057c79ff1fb7e763e5",
+    "url": "/catsProjectReact/static/js/runtime-main.6399bffb.js"
+  },
+  {
+    "revision": "47bf35aac826ba7b03e666d055efc70c",
+    "url": "/catsProjectReact/static/media/0.47bf35aa.png"
+  },
+  {
+    "revision": "987f06cb28ca09bdc17ac3f36b8c775b",
+    "url": "/catsProjectReact/static/media/1.987f06cb.jpg"
+  },
+  {
+    "revision": "26dd9259643ca9e30f54c2162b10b0f7",
+    "url": "/catsProjectReact/static/media/2.26dd9259.jpg"
+  },
+  {
+    "revision": "6b262e5cf717001e6b9baf86ee1b2440",
+    "url": "/catsProjectReact/static/media/3.6b262e5c.jpg"
+  },
+  {
+    "revision": "9aa2d90527a7089f943301caf004eafb",
+    "url": "/catsProjectReact/static/media/4.9aa2d905.jpg"
+  },
+  {
+    "revision": "613fc6f3b0a3258253ca364841eb6064",
+    "url": "/catsProjectReact/static/media/5.613fc6f3.jpg"
+  },
+  {
+    "revision": "0b0f56a55f724868f58056ff49b52e11",
+    "url": "/catsProjectReact/static/media/Croix.0b0f56a5.svg"
+  },
+  {
+    "revision": "ecea2d0a87a65e160243031277564e54",
+    "url": "/catsProjectReact/static/media/Menuico.ecea2d0a.svg"
+  },
+  {
+    "revision": "dc15452124ca838beb0ae6c52fdddcdb",
+    "url": "/catsProjectReact/static/media/Metroid.dc154521.mp3"
+  },
+  {
+    "revision": "f7814f7bc90a8c549f004655ef6abe37",
+    "url": "/catsProjectReact/static/media/Payout.f7814f7b.wav"
+  },
+  {
+    "revision": "51377e112e2b3a0b8650369aee9261ad",
+    "url": "/catsProjectReact/static/media/boxdig.51377e11.mp3"
+  },
+  {
+    "revision": "41bc6580fa5ffc705f6ed005c4c731a9",
+    "url": "/catsProjectReact/static/media/cat-154642.41bc6580.svg"
+  },
+  {
+    "revision": "6ae855697d43550d90de42abc206b574",
+    "url": "/catsProjectReact/static/media/chien.6ae85569.mp3"
+  }
+]);
