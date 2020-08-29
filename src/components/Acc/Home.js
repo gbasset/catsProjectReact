7fd6cfe3fea6cc99161c8d03e1a5fd0572c228c5
gaetan <gbassetprotonmail.com>
2020-08-29 11:37:16 +0200

    add infinityscroll

--- src/components/Acc/Home.js
+++ src/components/Acc/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import './Home.css'
+import CatsList from './../CasinoCats/CatsList';
 export default function Home() {
     return (
         <div className="contain">
-            <h1>HOME</h1>
-            <p>Welcome to the project Cats </p>
+            <h1>Welcome to the project Cats </h1>
+            <div className="presProject">
+                <div className="illustration">
+
+                </div>
+            </div>
         </div>
     )
 }
